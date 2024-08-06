9dce53f1274716ec8adb87776f1f4a60a3cd8a51
sebatustra <sebatustra@protonmail.com>
2024-08-05 20:26:40 -0400

    problemas endpoints

--- app/stock/views.py
+++ app/stock/views.py
@@ -1,5 +1,33 @@
-class MovimientoListCreateView:
-    pass
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import (
+    Lote,
+    Consumo,
+    Stock,
+    Movimiento
+)
+from .serializers import (
+    LoteSerializer,
+    ConsumoSerializer,
+    StockSerializer,
+    MovimientoSerializer
+)
+from rest_framework.decorators import api_view
+
+class MovimientoListCreateView(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = Movimiento.objects.all()
+        serializer = MovimientoSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = MovimientoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class MovimientoRetrieveDestroyView:
