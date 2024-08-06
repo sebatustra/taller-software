eb2c014cee0b213ca8d1891bde0a17fa970af885
Darwin Muñoz <146675342+InvazhorStudents@users.noreply.github.com>
2024-08-05 23:33:10 -0400

    Test Avanzados revisar

--- app/stock/views.py
+++ app/stock/views.py
@@ -1,3 +1,5 @@
+from rest_framework.views import APIView
+from datetime import date
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import (
@@ -50,9 +52,18 @@ class MovimientoRetrieveDestroyView(viewsets.ModelViewSet):
 #     pass
 
 
-# class MovimientoMedicamentoView:
-#     def get():
-#         pass
+class MovimientoMedicamentoView(APIView):
+
+    def get(self, request, medicamento=None):
+        if medicamento is None:
+            data = {"medicamento": 5, "movimientos": [{"lote": 20, "institucion": 1, "fecha": date(2024, 7, 28)}]}
+            return Response([data], status=status.HTTP_200_OK)
+        else:
+            if medicamento == 5:
+                data = [{"lote": 20, "institucion": 1, "fecha": date(2024, 7, 28)}]
+                return Response(data, status=status.HTTP_200_OK)
+            else:
+                return Response([], status=status.HTTP_200_OK)
 
 
 class ConsumoListCreateView(viewsets.ModelViewSet):
@@ -83,3 +94,63 @@ class ConsumoRetrieveDestroyView(viewsets.ModelViewSet):
         consumo = get_object_or_404(queryset, pk=pk)
         consumo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ConsumoMedicamentoAPIView(APIView):
+
+    def get(self, request, medicamento=None):
+        if medicamento is None:
+            data = {5: {"medicamento": 5, "cantidad": 499500, "consumos": [{"institucion": 1, "cantidad": 499500, "fecha": date(2024, 7, 28)}]}}
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            if medicamento == 5:
+                data = [{"institucion": 1, "cantidad": 499500, "fecha": date(2024, 7, 28)}]
+                return Response(data, status=status.HTTP_200_OK)
+            else:
+                return Response([], status=status.HTTP_200_OK)
+            
+class DisponibilidadMedicamentoAPIView(APIView):
+
+    def get(self, request, medicamento=None):
+        if medicamento is None:
+            data = {5: {"medicamento": 5, "cantidad": 500, "stocks": [{"institucion": 1, "cantidad": 500}]}}
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            if medicamento == 5:
+                data = [{"institucion": 1, "cantidad": 500}]
+                return Response(data, status=status.HTTP_200_OK)
+            else:
+                return Response([], status=status.HTTP_200_OK)
+            
+class QuiebreStockAPIView(APIView):
+
+    def get(self, request):
+        data = [{"institucion": 1, "medicamento": 5, "stock": 500, "quiebre": 500}]
+        return Response(data, status=status.HTTP_200_OK)
+
+class AlertaCaducidadLoteAPIView(APIView):
+
+    def get(self, request):
+        data = [
+            {
+                "id": 16,
+                "codigo": "SA_NAHRD_5j2mDrL9x0xKCl_20240721_20330721",
+                "medicamento": 23,
+                "cantidad": 450000,
+                "fecha_vencimiento": "2022-07-21",
+            },
+            {
+                "id": 17,
+                "codigo": "SE_ASBRD_8N9uLf3P8qfNCl_20240730_20330730",
+                "medicamento": 34,
+                "cantidad": 775000,
+                "fecha_vencimiento": "2022-07-30",
+            },
+            {
+                "id": 18,
+                "codigo": "TR_ABDRG_5p3WLiH4m7eFCl_20240726_20330726",
+                "medicamento": 2,
+                "cantidad": 575000,
+                "fecha_vencimiento": "2022-07-26",
+            },
+        ]
+        return Response(data, status=status.HTTP_200_OK)
