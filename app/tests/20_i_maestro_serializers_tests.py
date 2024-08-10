3ad5ad3fb9571e2a04e66517b1e151026fd70075
Darwin Muñoz <146675342+InvazhorStudents@users.noreply.github.com>
2024-08-10 10:18:57 -0400

    Test Maestro

--- app/tests/20_i_maestro_serializers_tests.py
+++ app/tests/20_i_maestro_serializers_tests.py
@@ -24,3 +24,150 @@ def test_equipamiento_serializer():
 @pytest.mark.django_db
 def test_quiebre_serializer():
     pass
+
+
+# from maestro.models import Institucion, Medicamento, Quiebre
+# from maestro.serializers import QuiebreSerializer
+# import pytest
+# import json
+
+# from maestro.models import Institucion, Item, Medicamento, Quiebre
+
+
+# @pytest.mark.django_db
+# def test_institucion_serializer():
+#     from maestro.serializers import InstitucionSerializer
+
+#     institucion = Institucion.objects.create(
+#         nombre="Hospital de San Antonio", tipo="hospital", titularidad="publico", num_camas_uti=10, num_camas_uci=5, factor=1.0
+#     )
+
+#     data = {
+#         "nombre": institucion.nombre,
+#         "tipo": institucion.tipo,
+#         "titularidad": institucion.titularidad,
+#         "num_camas_uti": institucion.num_camas_uti,
+#         "num_camas_uci": institucion.num_camas_uci,
+#     }
+
+#     serialized_data = InstitucionSerializer(data=data)
+#     serialized_object = InstitucionSerializer(institucion)
+#     serialized_data.is_valid()
+
+#     assert json.dumps(serialized_object.data) == json.dumps(
+#         data), "data serializada no tiene el orden correcto"
+#     assert serialized_data.errors == {}, f"Errores: {serialized_data.errors}"
+
+
+# @pytest.mark.django_db
+# def test_medicamento_serializer():
+#     from maestro.serializers import MedicamentoSerializer
+
+#     medicamento = Medicamento.objects.create(
+#         nombre_comercial="Paracetamol", nombre_generico="Paracetamol", ingredientes="Paracetamol", concentracion="500mg", forma_presentacion="frasco", forma_farmaceutica="tabletas", via_administracion="oral", indicaciones_terapeuticas="Dolor", contraindicaciones="Ninguna", efectos_secundarios="Ninguno", instrucciones_dosificacion="1 tableta cada 8 horas", fabricante="Bayer", informacion_almacenamiento="Lugar fresco y seco", interacciones_medicamentosas="Ninguna"
+#     )
+
+#     data = {
+#         "nombre_comercial": medicamento.nombre_comercial,
+#         "nombre_generico": medicamento.nombre_generico,
+#         "ingredientes": medicamento.ingredientes,
+#         "concentracion": medicamento.concentracion,
+#         "forma_presentacion": medicamento.forma_presentacion,
+#         "forma_farmaceutica": medicamento.forma_farmaceutica,
+#         "via_administracion": medicamento.via_administracion,
+#         "indicaciones_terapeuticas": medicamento.indicaciones_terapeuticas,
+#         "contraindicaciones": medicamento.contraindicaciones,
+#         "efectos_secundarios": medicamento.efectos_secundarios,
+#         "instrucciones_dosificacion": medicamento.instrucciones_dosificacion,
+#         "fabricante": medicamento.fabricante,
+#         "informacion_almacenamiento": medicamento.informacion_almacenamiento,
+#         "interacciones_medicamentosas": medicamento.interacciones_medicamentosas,
+#     }
+
+#     serialized_data = MedicamentoSerializer(data=data)
+#     serialized_object = MedicamentoSerializer(medicamento)
+#     serialized_data.is_valid()
+
+#     assert json.dumps(serialized_object.data) == json.dumps(
+#         data), "data serializada no tiene el orden correcto"
+#     assert serialized_data.errors == {}, f"Errores: {serialized_data.errors}"
+
+
+# @pytest.mark.django_db
+# def test_item_serializer():
+#     from maestro.serializers import ItemSerializer
+
+#     item = Item.objects.create(
+#         nombre="Monitor de signos vitales", tipo="apoyo_monitorizacion"
+#     )
+
+#     data = {
+#         "nombre": item.nombre,
+#         "tipo": item.tipo,
+#     }
+
+#     serialized_data = ItemSerializer(data=data)
+#     serialized_object = ItemSerializer(item)
+#     serialized_data.is_valid()
+
+#     assert json.dumps(serialized_object.data) == json.dumps(
+#         data), "data serializada no tiene el orden correcto"
+#     assert serialized_data.errors == {}, f"Errores: {serialized_data.errors}"
+
+
+# @pytest.mark.django_db
+# def test_equipamiento_serializer():
+#     from maestro.serializers import EquipamientoSerializer
+
+#     item = Item.objects.create(
+#         nombre="Monitor de signos vitales", tipo="apoyo_monitorizacion"
+#     )
+
+#     equipamiento = item.equipamiento_set.create(
+#         marca="Bosch", modelo="2021"
+#     )
+
+#     data = {
+#         "item": item.id,
+#         "marca": equipamiento.marca,
+#         "modelo": equipamiento.modelo,
+#     }
+
+#     serialized_data = EquipamientoSerializer(data=data)
+#     serialized_object = EquipamientoSerializer(equipamiento)
+#     serialized_data.is_valid()
+
+#     assert json.dumps(serialized_object.data) == json.dumps(
+#         data), "data serializada no tiene el orden correcto"
+#     assert serialized_data.errors == {}, f"Errores: {serialized_data.errors}"
+
+
+# @pytest.mark.django_db
+# def test_quiebre_serializer():
+#     institucion = Institucion.objects.create(
+#         nombre="Hospital de Talca", tipo="hospital", titularidad="publico", num_camas_uti=10, num_camas_uci=5, factor=1.0
+#     )
+
+#     medicamento = Medicamento.objects.create(
+#         nombre_comercial="Panadol", nombre_generico="Panadol", ingredientes="Panadol", concentracion="500mg", forma_presentacion="frasco", forma_farmaceutica="tabletas", via_administracion="oral", indicaciones_terapeuticas="Dolor", contraindicaciones="Ninguna", efectos_secundarios="Ninguno", instrucciones_dosificacion="1 tableta cada 8 horas", fabricante="Bayer", informacion_almacenamiento="Lugar fresco y seco", interacciones_medicamentosas="Ninguna"
+#     )
+
+#     # Verifica si ya existe un objeto Quiebre con la misma institucion y medicamento
+#     quiebre, created = Quiebre.objects.get_or_create(
+#         institucion=institucion, medicamento=medicamento, defaults={
+#             'cantidad': 500}
+#     )
+
+#     data = {
+#         "institucion": institucion.id,
+#         "medicamento": medicamento.id,
+#         "cantidad": quiebre.cantidad,
+#     }
+
+#     serialized_data = QuiebreSerializer(data=data)
+#     serialized_object = QuiebreSerializer(quiebre)
+#     is_valid = serialized_data.is_valid()
+
+#     assert is_valid, f"Errores: {serialized_data.errors}"
+#     assert json.dumps(serialized_object.data) == json.dumps(
+#         data), "data serializada no tiene el orden correcto"
--- app/tests/2_b_maestro_models_tests.py
+++ app/tests/2_b_maestro_models_tests.py
@@ -1,3 +1,190 @@
+# from django.db import IntegrityError
+# import pytest
+
+
+# @pytest.mark.django_db
+# def test_institucion_model():
+#     from maestro.models import Institucion
+
+#     nombre = "Hospital Clínico"
+#     tipo = Institucion.Tipo.HOSPITAL_CLINICO
+#     titularidad = Institucion.Titularidad.PUBLICO
+#     num_camas_uti = 10
+#     num_camas_uci = 5
+#     factor = 1.5
+
+#     institucion = Institucion.objects.create(
+#         nombre=nombre,
+#         tipo=tipo,
+#         titularidad=titularidad,
+#         num_camas_uti=num_camas_uti,
+#         num_camas_uci=num_camas_uci,
+#         factor=factor,
+#     )
+
+#     assert institucion.nombre == nombre
+#     assert institucion.tipo == tipo
+#     assert institucion.titularidad == titularidad
+#     assert institucion.num_camas_uti == num_camas_uti
+#     assert institucion.num_camas_uci == num_camas_uci
+#     assert institucion.factor == factor
+
+#     assert str(
+#         institucion) == nombre, "se debe usar el nombre como representación str del objeto"
+
+#     institucion.num_camas_uci = -5
+#     institucion.num_camas_uti = -10
+
+#     with pytest.raises(IntegrityError):
+#         institucion.save()
+
+#         institucion = Institucion.objects.get(id=institucion.id)
+#         assert institucion.num_camas_uci >= 0, "camas UCI debe ser mayor o igual que cero"
+#         assert institucion.num_camas_uti >= 0, "camas UTI debe ser mayor o igual que cero"
+
+
+# @pytest.mark.django_db
+# def test_medicamento_model():
+#     from maestro.models import Medicamento
+
+#     nombre_comercial = "Paracetamol"
+#     nombre_generico = "Acetaminofén"
+#     ingredientes = "Paracetamol"
+#     concentracion = "500 mg"
+#     forma_presentacion = Medicamento.FormaPresentacion.FRASCO
+#     forma_farmaceutica = Medicamento.FormaFarmaceutica.TABLETAS
+#     via_administracion = Medicamento.Via.ORAL
+#     indicaciones_terapeuticas = "Dolor y fiebre"
+#     contraindicaciones = "Alergia al paracetamol"
+#     efectos_secundarios = "Ninguno"
+#     instrucciones_dosificacion = "1 tableta cada 6 horas"
+#     fabricante = "Bayer"
+#     informacion_almacenamiento = "Lugar fresco y seco"
+#     interacciones_medicamentosas = "Ninguna"
+
+#     medicamento = Medicamento.objects.create(
+#         nombre_comercial=nombre_comercial,
+#         nombre_generico=nombre_generico,
+#         ingredientes=ingredientes,
+#         concentracion=concentracion,
+#         forma_presentacion=forma_presentacion,
+#         forma_farmaceutica=forma_farmaceutica,
+#         via_administracion=via_administracion,
+#         indicaciones_terapeuticas=indicaciones_terapeuticas,
+#         contraindicaciones=contraindicaciones,
+#         efectos_secundarios=efectos_secundarios,
+#         instrucciones_dosificacion=instrucciones_dosificacion,
+#         fabricante=fabricante,
+#         informacion_almacenamiento=informacion_almacenamiento,
+#         interacciones_medicamentosas=interacciones_medicamentosas,
+#     )
+
+#     assert medicamento.nombre_comercial == nombre_comercial
+#     assert medicamento.nombre_generico == nombre_generico
+#     assert medicamento.ingredientes == ingredientes
+#     assert medicamento.concentracion == concentracion
+#     assert medicamento.forma_presentacion == forma_presentacion
+#     assert medicamento.forma_farmaceutica == forma_farmaceutica
+#     assert medicamento.via_administracion == via_administracion
+#     assert medicamento.indicaciones_terapeuticas == indicaciones_terapeuticas
+#     assert medicamento.contraindicaciones == contraindicaciones
+#     assert medicamento.efectos_secundarios == efectos_secundarios
+#     assert medicamento.instrucciones_dosificacion == instrucciones_dosificacion
+#     assert medicamento.fabricante == fabricante
+#     assert medicamento.informacion_almacenamiento == informacion_almacenamiento
+#     assert medicamento.interacciones_medicamentosas == interacciones_medicamentosas
+
+#     assert str(
+#         medicamento) == f"{nombre_comercial} ({nombre_generico}) | {fabricante}", "se debe usar el nombre comercial y genérico como representación str del objeto"
+
+
+# @pytest.mark.django_db
+# def test_item_model():
+#     from maestro.models import Item
+
+#     nombre = "Monitor de signos vitales"
+#     tipo = Item.Tipo.APOYO_MONITORIZACION
+
+#     item = Item.objects.create(
+#         nombre=nombre,
+#         tipo=tipo,
+#     )
+
+#     assert item.nombre == nombre
+#     assert item.tipo == tipo
+
+#     assert str(
+#         item) == f"{nombre} ({tipo})", "se debe usar el nombre y tipo como representación str del objeto"
+
+
+# """
+# class Equipamiento(models.Model):
+#     class Tipo(models.TextChoices):
+#         SOPORTE_VITAL = "soporte_vital", "Soporte Vital"
+#         APOYO_MONITORIZACION = "apoyo_monitorizacion", "Apoyo y Monitorización"
+
+#     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+#     marca = models.CharField(max_length=255)
+#     modelo = models.CharField(max_length=255)
+
+#     def __str__(self) -> str:
+#         return f"{self.modelo} ({self.modelo}) | {self.item}"
+
+
+# """
+
+
+# @pytest.mark.django_db
+# def test_equipamiento_model():
+#     from maestro.models import Equipamiento, Item
+
+#     item = Item.objects.all().first()
+#     marca = "Philips"
+#     modelo = "M3"
+
+#     equipamiento = Equipamiento.objects.create(
+#         item=item,
+#         marca=marca,
+#         modelo=modelo,
+#     )
+
+#     assert equipamiento.item == item
+#     assert equipamiento.marca == marca
+#     assert equipamiento.modelo == modelo
+
+#     # Incidencia: modelo se repite en lugar de marca
+#     assert str(
+#         equipamiento) == f"{modelo} ({modelo}) | {item}", "se debe usar el modelo y marca como representación str del objeto"
+
+
+# @pytest.mark.django_db
+# def test_quiebre_model():
+#     from maestro.models import Quiebre, Institucion, Medicamento
+
+#     institucion = Institucion.objects.all().first()
+#     medicamento = Medicamento.objects.all().first()
+
+#     quiebre = Quiebre.objects.create(
+#         institucion=institucion,
+#         medicamento=medicamento,
+#         cantidad=500,
+#     )
+
+#     assert quiebre.institucion == institucion
+#     assert quiebre.medicamento == medicamento
+#     assert quiebre.cantidad == 500
+
+#     with pytest.raises(IntegrityError):
+#         Quiebre.objects.create(
+#             institucion=institucion,
+#             medicamento=medicamento,
+#             cantidad=-500,
+#         )
+
+#         quiebre = Quiebre.objects.get(id=quiebre.id)
+#         assert quiebre.cantidad >= 0, "cantidad debe ser mayor o igual que cero"
+
+
 import pytest
 
 
